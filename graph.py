321387af7ed6f1d72a983bb829088e8e223e779a
Domenic Murtari <domenic.murtari@gmail.com>
2014-10-03 13:32:53 -0600

    Cleanup

--- graph.py
+++ graph.py
@@ -12,11 +12,13 @@ class Graph:
     def __iter__(self):
         return iter(self.node_list.values())
     
-    # Import a graph from a file. Each line should contain the two nodes that
-    # an edge goes between, separated by a tab.
+    # Initialize a graph from a file representing a 2D matrix with nodes
+    # separeted by spaces. 
     def import_from_file(self, graph_file, reverse = False):
         neighborhood_file = open(graph_file, "r")
         neighborhood = []
+        edgecount = 0
+
         for line in neighborhood_file:
             neighborhood.append(line.strip().split(" "))
 
@@ -24,18 +26,19 @@ class Graph:
             for j, house in enumerate(neighbors):
                 print i, j, house
                 if j != (len(neighbors) - 1):
-                    print "Adding edge ", house, i, j, " and ", i, j + 1
                     self.add_edge(house, neighbors[j + 1], 0) 
+                    edgecount += 1
                 if j != 0:
-                    print "Adding edge ", house, i, j, " and ", i, j - 1
                     self.add_edge(house, neighbors[j - 1], 0) 
+                    edgecount += 1
                 if i != (len(neighbors) - 1):
-                    print "Adding edge ", house, i, j, " and ", i + 1, j
                     self.add_edge(house, neighborhood[i + 1][j], 0)
+                    edgecount += 1
                 if i != 0:
-                    print "Adding edge ", house, i, j, " and ", i - 1, j
                     self.add_edge(house, neighborhood[i - 1][j], 0) 
-                
+                    edgecount += 1
+
+        print "Added ", edgecount, " edges"
 
     # Add a node of a given name, and increment the amount of nodes
     def add_node(self, name):
